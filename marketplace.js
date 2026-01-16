ec43cd90c3cb356befe9bea7a56ec9486cab73cf
Allen Shibu <allenshibukuriakose@gmail.com>
2026-01-16 22:22:24 +0530

    wishlist page functions completed

--- marketplace.js
+++ marketplace.js
@@ -136,11 +136,11 @@ function showAdNotification() {
       }, 2000);
     }
     
-    showNotification()
+
 
 
 function AddtoLocalStorage(product) {
-      let WishList = JSON.parse(localStorage.getItem("MyWishlist"))||[];
+      const WishList = JSON.parse(localStorage.getItem("MyWishlist"))||[];
       console.log(WishList);
 
       const exists = WishList.some((item) => item.title === product.title); //.some() loops through the array and returns true if it finds a match.
@@ -160,7 +160,7 @@ function AddtoLocalStorage(product) {
       
     }
 
-    AddtoLocalStorage(product);
+
     
 
 const WishGrid = document.querySelector('.wishlist-grid')
@@ -169,21 +169,21 @@ if (WishGrid) {
   const WishList = JSON.parse(localStorage.getItem("MyWishlist")) || [];
 
   if(WishList.length == 0){
-    WishGrid.innerHTML = `<p class="flex text-gray-500 items-center justify-center text-2xl ">Your wishlist is empty.....</p>`;
+    WishGrid.innerHTML = `<p class="flex text-gray-500 text-2xl ">Your wishlist is empty.....</p>`;
   }
   else {
-    WishGrid.forEach((product, index) => {
+    WishList.forEach((product, index) => {
       const card = document.createElement('div')
       
-      card.innerHTML = `<div class="relative overflow-hidden rounded-2xl">
-                    <img src="${product.image}" class="w-full h-64 object-cover">
+      card.innerHTML = `<div class="flex flex-row relative overflow-hidden rounded-2xl">
+                    <img src="${product.image}" class="w-full h-64 object-cover group-hover:scale-105 transition-transform duration-300">
                     
                     <button onclick="removeFromWishlist(${index})" class="absolute top-3 right-3 bg-white p-2 rounded-full text-red-500 shadow-sm hover:bg-red-100">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                         </svg>
                     </button>
-                </div>
+                  </div>
                 <div class="mt-3">
                     <p class="font-bold text-lg dark:text-white">${product.title}</p>
                     <span class="font-bold text-lg text-green-600">${product.price}</span>
@@ -193,3 +193,16 @@ if (WishGrid) {
     })
   }
 }
+
+window.removeFromWishlist = function (index) {
+  let Wishlist = JSON.parse(localStorage.getItem("MyWishlist")) || [];
+
+  //removes 1 item at the specific index
+  Wishlist.splice(index, 1);
+
+  // Save the new list back to storage
+  localStorage.setItem("MyWishlist", JSON.stringify(Wishlist));
+
+  // Reload the page to see the changes
+  location.reload();
+};
