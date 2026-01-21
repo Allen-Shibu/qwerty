9865ab4ba5a1df41fc38615028deccbfdaf7b7f9
Allen Shibu <allenshibukuriakose@gmail.com>
2026-01-21 22:32:13 +0530

    some changes

---
+++ index.js
@@ -0,0 +1,75 @@
+import { supabase } from "./supabaseClient.js";
+
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const repasswordInput = document.getElementById("repassword");
+const SignupBtn = document.querySelector(".registerbtn");
+const LoginBtn = document.getElementById("loginbtn");
+const Failnotify = document.getElementById("fail-notification");
+
+async function signup() {
+    const email = emailInput.value;
+    if (!email.endsWith("@gectcr.ac.in")) {
+        alert("Access Denied: You must use a valid GEC College email (@gectcr.ac.in)");
+        return;
+    }
+  const password = passwordInput.value;
+
+  const { data, error } = await supabase.auth.signUp({
+    email: email,
+    password: password,
+  });
+
+  if (error) {
+    alert("Sign Up Failed!", error.message);
+  } else {
+    alert("Success!");
+    window.location.href = "market-place.html";
+  }
+}
+
+async function login() {
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: email,
+    password: password,
+  });
+
+  if (error) {
+    console.log("Supabase Error", error);
+    FailNotification(); 
+    
+  } else {
+    alert("Login successful");
+
+    window.location.href ="market-place.html";
+  }
+}
+
+if (SignupBtn)
+  SignupBtn.addEventListener("click", (e) => {
+    e.preventDefault(); //stops from refreshing the page
+    signup();
+  });
+
+if (LoginBtn)
+  LoginBtn.addEventListener("click", (e) => {
+    e.preventDefault(); //stops from refreshing the page
+    login();
+  });
+
+function FailNotification() {
+  Adnotify.classList.remove("hidden");
+
+  if (timeoutId) {
+    //clear any existing timer
+    clearTimeout(timeoutId);
+  }
+
+  timeoutId = setTimeout(() => {
+    Failnotify.classList.add("hidden");
+  }, 2000);
+}
--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 include ('managedb.php');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['signup'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    header("Location: market-place.html");
+    exit();
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
