bad67bcd67fd81b0ff59d45cce85a761035308ea
Allen Shibu <allenshibukuriakose@gmail.com>
2026-01-24 17:34:44 +0530

    auth+emailverification

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const SignupBtn = document.querySelector(".registerbtn");
 const LoginBtn = document.getElementById("loginbtn");
 const Failnotify = document.getElementById("fail-notification");
 
+
 async function signup() {
     const email = emailInput.value;
     if (!email.endsWith("@gectcr.ac.in")) {
@@ -15,17 +16,30 @@ async function signup() {
         return;
     }
   const password = passwordInput.value;
+  const siteUrl = window.location.origin;
+      console.log(siteUrl);
 
   const { data, error } = await supabase.auth.signUp({
     email: email,
     password: password,
+    options: {
+      emailRedirectTo: `${siteUrl}/market-place.html`,
+    },
   });
 
   if (error) {
-    alert("Sign Up Failed!", error.message);
+    alert("Sign Up Failed!" + error.message);
   } else {
-    alert("Success!");
-    window.location.href = "market-place.html";
+    
+    alert(
+      "Verification email sent! Please check your inbox to activate your account.",
+      
+    );
+    
+    nameInput.value = ""
+    repasswordInput.value=""
+    emailInput.value = "";
+    password.value = "";
   }
 }
 
@@ -39,13 +53,12 @@ async function login() {
   });
 
   if (error) {
-    console.log("Supabase Error", error);
+    console.log("Login Failed!"+error.message);
     FailNotification(); 
     
   } else {
-    alert("Login successful");
+    window.location.href="market-place.html"
 
-    window.location.href ="market-place.html";
   }
 }
 
