4141afdaa70734d35251f12238301b4323777412
Allen Shibu <allenshibukuriakose@gmail.com>
2026-01-27 23:49:19 +0530

    listing page functional

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,9 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         login: resolve(__dirname, "login.html"),
-        market: resolve(__dirname, "market-place.html"),
+        main: resolve(__dirname, "market-place.html"),
         selling: resolve(__dirname, "sellingpage.html"),
+        listing: resolve(__dirname, "listing.html"),
       },
     },
   },
